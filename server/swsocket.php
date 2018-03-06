a74c2f832235a28f38c76526bc9022d555827089
Shaozhuo <Zheng-Shaozhuo@outlook.com>
2018-03-06 15:28:24 +0800

    完善代码逻辑，新增代码注释

--- server/swsocket.php
+++ server/swsocket.php
@@ -1,43 +1,39 @@
 <?php
     $sw = new swoole_websocket_server('0.0.0.0', 9502);
+	
+	// 设置链接属性
     $sw->set(array(
-    		'daemonize' => false,
-    		'max_request' => 10000,
-    		'dispatch_mode' => 2,
+    		'daemonize' => false,		// 是否守护进程, 即是否后台运行
+    		'max_request' => 10000,		// 最大连接数量
+    		'dispatch_mode' => 2,		// 派发模式
     		'debug_mode' => 1,
+			//心跳检测
     		'heartbeat_checkk_interval' => 5,
     		'heartbeat_idle_time' => 600
     ));
     
-    $sw->on('open', function($sw, $request) {
+	// 监听websocket长连接建立
+    $sw->on('open', function(swoole_websocket_server $server, $request) {
     	echo "func open tick [$request->fd].\n";
-    	$sw->push($request->fd, "hello, welcome to chatroom \n");
+    	$server->push($request->fd, "hello, welcome to chatroom \n");
     });
     
-    $sw->on('message', function($sw, $request) {
-    	echo "func message tick [$request->fd], receive msg.\n";
-    	$msg = 'from ' . $request->fd . " : {$request->data}\n";
+	// 监听websocket消息事件
+    $sw->on('message', function(swoole_websocket_server $server, $frame) {
+    	echo "func message tick [$frame->fd]:[$frame->data]:[$frame->opcode]:[$frame->finish], receive msg.\n";
+    	$msg = 'No.' . $frame->fd . " (" . date('Y-m-d H:i:s') . ") : {$frame->data}\n";
     	
-    	$start_fd = 0;
-    	while (true) {
-    		$conn_list = $sw->connection_list($start_fd, 100);
-    		var_dump($conn_list, count($conn_list));
-    		
-    		if ($conn_list == false || count($conn_list) == 0) {
-    			echo "finish\n";
-    			return ;
-    		}
-    		
-    		$start_fd = end($conn_list);
-    		foreach($conn_list as $fd) {
-    			$sw->push($fd, $msg);
-    		}
-    	};
+    	global $sw;
+		// 遍历当前所有链接, 群发推送
+		foreach($sw->connections as $fd) {
+    		$server->push($fd, $msg);
+    	}
     });
     
-    $sw->on('close', function($sw, $fd){
+	// 监听websocket链接关闭事件
+    $sw->on('close', function($serv, $fd){
     	echo "client [$fd] is closed.\n";
-    	$sw->close($fd);
+    	$serv->close($fd);
     });
     
     $sw->start();
